a143286b0a332e07492acd1e40e399612be568ba
vimi8 <466824141>
2017-03-29 15:46:56 +0800

    yyx

--- app/src/main/java/com/yyx/nineimage/fragment/NineImgFragment.java
+++ app/src/main/java/com/yyx/nineimage/fragment/NineImgFragment.java
@@ -7,7 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lzy.ninegrid.NineGridView;
 import com.yyx.nineimage.R;
+import com.yyx.nineimage.utils.NineImgUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vimi8 on 2017/3/29.
@@ -24,7 +29,24 @@ public class NineImgFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view =inflater.inflate(R.layout.activity_main,container,false);
+        view =inflater.inflate(R.layout.fragment_nine_img,container,false);
+        NineGridView nine1 = (NineGridView) view.findViewById(R.id.nine_1);
+        List<String> imgList1 = new ArrayList<>();
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf807.png");
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf80a.png");
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf80b.png");
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf80f.png");
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf80f.png");
+        imgList1.add("http://cnews.chinadaily.com.cn/img/attachement/png/site1/20170323/a41f726b573a1a3d7bf810.png");
+        NineImgUtils.funNineGridView(getContext(),imgList1,nine1);
+
+        NineGridView nine2 = (NineGridView) view.findViewById(R.id.nine_2);
+        List<String> imgList2 = new ArrayList<>();
+        imgList2.add("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1018122506,3421976594&fm=23&gp=0.jpg");
+        imgList2.add("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3698625380,1448089034&fm=23&gp=0.jpg");
+        imgList2.add("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3698625380,1448089034&fm=23&gp=0.jpg");
+        imgList2.add("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3999788847,752654998&fm=23&gp=0.jpg");
+        NineImgUtils.funNineGridView(getContext(),imgList2,nine2);
         return view;
     }
 }
